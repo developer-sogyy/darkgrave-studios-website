3afa959e7b5a88ecd4b5cd69a421d38a00279896
developer-sogyy <122156533+developer-sogyy@users.noreply.github.com>
2024-08-31 22:22:16 +0200

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,57 @@
+function openItch() {
+    window.open("https://darkgrave-studios.itch.io/shop-of-violence");
+}
+
+function openGooglePlay() {
+    window.open("https://play.google.com/store/apps/details?id=com.ShopofViolenceStudios.ShopofViolence");
+}
+
+
+
+
+function nikolaDS() {
+    window.open("https://www.discord.com/users/790862986813898782");
+}
+
+function nikolaIG ()  {
+    window.open("https://www.instagram.com/crhaxx_mtb/");
+}
+
+function nikolaTT() {
+    window.open("https://www.tiktok.com/@crhaxx_mtb");
+}
+
+
+
+function adamDS() {
+    window.open("https://www.discord.com/users/1015600111499358318");
+}
+
+function adamIG() {
+    window.open("https://www.instagram.com/adam.bedny/");
+}
+
+function adamTT() {
+    window.open("https://www.tiktok.com/@arpad_1234");
+}
+
+
+
+
+function danielDS() {
+    window.open("https://www.discord.com/users/1164915613697855528")
+}
+
+function danielTT() {
+    window.open("https://www.tiktok.com/@notyouofficial1");
+}
+
+
+
+function stepanDS() {
+    window.open("https://www.discord.com/users/616875506880479235");
+}
+
+function stepanIG() {
+    window.open("https://www.instagram.com/stepancrhak/");
+}
